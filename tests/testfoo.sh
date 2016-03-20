febe5b4c31c3c76daa08407701ca393aca412a6f
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-03-20 15:42:54 +0100

    update test

--- tests/testfoo.sh
+++ tests/testfoo.sh
@@ -1,5 +1,5 @@
 set -e # stop on first error
 # ./make.sh
 qtest -o footest.ml $@ extract foo.ml
-ocamlbuild -cflags -warn-error,+26 -use-ocamlfind -pkg oUnit,QTest2Lib footest.native
+ocamlbuild -cflags -warn-error,+26 -use-ocamlfind -pkg oUnit,qcheck footest.native
 ./footest.native
